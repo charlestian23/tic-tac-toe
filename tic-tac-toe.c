ee54e0a2e3ec2d5be30016ef5de1a974e3d54ecf
charlestian23 <charles.tian23@gmail.com>
2022-10-14 19:32:02 -0400

    Update tic-tac-toe.c
    More progress made

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -88,12 +88,49 @@ int is_tie(char** board, int size, int moveRow, int moveColumn)
   return !is_win(board, size, moveRow, moveColumn);
 }
 
+char swap_piece(char current_piece)
+{
+  if (current_piece == 'X')
+    return 'O';
+  return 'X';
+}
+
 int main()
 {
   int size = 3;
   char **board = create_board(size);
+  int move_counter = 1;
+  char current_piece = 'X';
+
+  // Assume that the players enter valid integers
+  while (move_counter < size * size)
+  {
+    int player_number = move_counter % 2;
+    int row = -1;
+    int column = -1;
+    printf("Player %d, enter a row: ", player_number);
+    scanf("%d", &row);
+    while (row < 0 || row > size)
+    {
+      printf("Row %d is not a valid row. Please try again...\n", row);
+      printf("Player %d, enter a different row: ", player_number);
+      scanf("%d", &row);
+    }
+    printf("Player %d, enter a column: ", player_number);
+    scanf("%d", &column);
+    while (column < 0 || column > size)
+    {
+      printf("Column %d is not a valid row. Please try again...\n", row);
+      printf("Player %d, enter a different column: ", player_number);
+      scanf("%d", &column);
+    }
+    printf("%d %d\n", row, column);
+  }
+
   print_board(board, size);
   for (int i = 0; i < size; i++)
     free(board[i]);
   free(board);
+
+  return EXIT_SUCCESS;
 }
