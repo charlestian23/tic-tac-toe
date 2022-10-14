e4c3a1258d372c7f908dacdfee9a3d473d424beb
charlestian23 <charles.tian23@gmail.com>
2022-10-14 16:37:19 -0400

    Added win and tie game checkers

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 
+#define true 1 
+#define false 0 
+
 char **create_board(int size)
 {
   char **board = (char **)calloc(size, sizeof(char *));
@@ -36,6 +39,38 @@ void print_board(char **board, int size)
       printf("\n");
     }
   }
+}
+
+int is_win(char** board, int size, int moveRow, int moveColumn)
+{
+  char symbol = board[moveRow][moveColumn];
+
+  // Check row
+  for (int i = 0; i < size; i++)
+    if (board[moveRow][i] != symbol)
+      return false;
+  
+  // Check column
+  for (int i = 0; i < size; i++)
+    if (board[i][moveColumn] != symbol)
+      return false;
+
+  // Check diagonal (if applicable)
+  if (moveRow == moveColumn)
+    for (int i = 0; i < size; i++)
+      if (board[i][i] != symbol)
+        return false;
+
+  return true;
+}
+
+int is_tie(char** board, int size, int moveRow, int moveColumn)
+{
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++)
+      if (board[i][j] == ' ')
+        return false;
+  return !is_win(board, size, moveRow, moveColumn);
 }
 
 int main()
